f4ec6a173c86597790e1d7c3c63fc6d4f9b890cc
jesvin1010 <jesvins27@gmail.com>
2025-12-04 12:04:36 +0530

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,4 +132,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#this is the change
